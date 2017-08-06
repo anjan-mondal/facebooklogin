8bd14ac1f26dd41983082554e4d7da8f07bc03f6
anjan <anjanmondal@gmail.com>
2017-08-06 16:40:54 +0530

    test

--- PiSelenium/src/com/selenium/SeleniumPiTest.java
+++ PiSelenium/src/com/selenium/SeleniumPiTest.java
@@ -1,19 +1,21 @@
 package com.selenium;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
 
 public class SeleniumPiTest {
 
-  public static RemoteWebDriver driver;
+  public static WebDriver driver;
 
-  public static void main(String[] args) {
+ @Test
+  public void test1() {
     DesiredCapabilities capabilities = DesiredCapabilities.firefox();
     System.setProperty("webdriver.gecko.driver", "/usr/local/bin/geckodriver");
     capabilities.setCapability("marionette", false);
